3a89c60ea9b9029ff6b32a4a1d9b7ef756379e09
Yograj Shisode <yograjshisode@gmail.com>
2024-05-24 10:14:48 +0530

    Added support for OpenAI embeddingd

--- embeddings/local_embedding.py
+++ embeddings/local_embedding.py
@@ -5,6 +5,7 @@
 # https://huggingface.co/spaces/mteb/leaderboard
 import os
 from langchain_community.embeddings import SentenceTransformerEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 # Vectore Stores
 # Facebook AI Similarity Search is a vectore store
@@ -32,10 +33,11 @@ class LocalEmbedding:
             source_files_path = "../guides"
             local_embedding_path = "../embedding_index"
 
-            le = LocalEmbedding(source_files_path, local_embedding_path)
+            le = LocalEmbedding(source_files_path, local_embedding_path, "FAISS", "SentenceTransformer")
             le.load_embeddings("Directory")
     """
-    def __init__(self, source_files_path: str, local_embedding_path: str, vstore_type: str, model_name="all-mpnet-base-v2"):
+    def __init__(self, source_files_path: str, local_embedding_path: str, vstore_type: str,
+                 embedding_type, model_name="all-mpnet-base-v2"):
         """Initialization
         Args:
             source_files_path (str): Path to the source documents
@@ -46,11 +48,12 @@ class LocalEmbedding:
         self.source_files_path = source_files_path
         self.local_embedding_path = local_embedding_path
         self.model_name = model_name
-        self.vstore = self._get_vectorestore()
+        self.vstore = self._get_vectorestore(vstore_type)
+        self.embeddings = self._get_embeddings(embedding_type)
         self.vectorestore = None
 
     def _get_vectorestore(self, vstore_type: str):
-        """_summary_
+        """Factory method to ge the different vectorstores
 
         Args:
             vstore_type (str): Supportted vstore types [Chroma, FAISS] 
@@ -68,37 +71,53 @@ class LocalEmbedding:
         else:
             raise Exception(f"Vectore Store {vstore_type} not supportted")
 
-    def load_embeddings_from_disk(self, embedding: SentenceTransformerEmbeddings):
-        """Loads prebviously embedded directly from disk
+    def _get_embeddings(self, embedding_type: str):
+        """Factory method to get embedding model
+
         Args:
-            embedding (SentenceTransformerEmbeddings): Haggingface Embedding Model
+            embedding_type (str): Supportted embedding types are [OpenAI, SentenceTransformer] 
+
+        Raises:
+            Exception: Raise exception if embedding_type is not suppotted
+
+        Returns:
+            Embeddings: Embeddings
         """
-        print(f"Loading local FAISS index from {self.local_embedding_path}")
-        self.vectorestore = FAISS.load_local(self.local_embedding_path, embedding,
+        if embedding_type == "OpenAI":
+            return OpenAIEmbeddings()
+        elif embedding_type == "SentenceTransformer":
+            return SentenceTransformerEmbeddings(model_name=self.model_name)
+        else:
+            raise Exception(f"Embeddings of type {embedding_type} not supportted")
+
+    def load_embeddings_from_disk(self):
+        """Loads prebviously embedded directly from disk
+        """
+        print(f"Loading local index from {self.local_embedding_path}")
+        self.vectorestore = self.vstore.load_local(self.local_embedding_path, self.embeddings,
                                                  allow_dangerous_deserialization=True)
         print("done.")
 
-    def load_and_embed_docs(self, loader_type: str, embedding: SentenceTransformerEmbeddings):
+    def load_and_embed_docs(self, loader_type: str):
         """Load the documents from source_files_path and store embedding in vectore stores
         Args:
             loader_type (str): Any langchain supportted loader
-            embedding (SentenceTransformerEmbeddings): Haggingface Embedding Model
         """
         print("Loading documents")
         loader = Loader(self.source_files_path, loader_type)
         loader.load_source()
-        print(f"Building FAISS index from documents")
+        print(f"Building index from documents")
         text_splitter = RecursiveCharacterTextSplitter(
                 chunk_size=500, 
                 chunk_overlap=75
                 )
         frags = text_splitter.split_documents(loader.docs)
         print(f"Poplulating vector store with {len(loader.docs)} docs in {len(frags)} fragments")
-        self.vectorestore = FAISS.from_documents(frags, embedding)
+        self.vectorestore = self.vstore.from_documents(frags, self.embeddings)
         print(f"Persisting vector store to: {self.local_embedding_path}")
         # Save embedded docs locally
         self.vectorestore.save_local(self.local_embedding_path)
-        print(f"Saved FAISS index to {self.local_embedding_path}")
+        print(f"Saved index to {self.local_embedding_path}")
 
     def load_embeddings(self, loader_type: str):
         """Load the embeddings if already exists at local_embedding_path
@@ -108,9 +127,8 @@ class LocalEmbedding:
             loader_type (str): Supportted loader types [Directory, PDF]
         """
         # We can use force_download=True to force a new download for model
-        embedding = SentenceTransformerEmbeddings(model_name=self.model_name)
         # print(f"Embedding: {embedding.e}")
         if os.path.exists(self.local_embedding_path):
-            self.load_embeddings_from_disk(embedding)
+            self.load_embeddings_from_disk()
         else:
-            self.load_and_embed_docs(loader_type, embedding)
+            self.load_and_embed_docs(loader_type)
